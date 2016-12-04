9275a1972840e1ea81f0fe52482274f3d0098569
Dan DaLuga <dan@daluga.com>
2016-12-04 00:00:52 -0600

    Removed unused imports.

--- src/main/java/com/daluga/messaging/consumer/service/Consumer.java
+++ src/main/java/com/daluga/messaging/consumer/service/Consumer.java
@@ -2,7 +2,6 @@ package com.daluga.messaging.consumer.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
