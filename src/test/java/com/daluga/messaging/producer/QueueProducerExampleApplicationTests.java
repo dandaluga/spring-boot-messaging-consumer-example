ff321b1245c73240e3f9407cba8dc999f2d4753b
Dan DaLuga <dan@daluga.com>
2019-07-20 11:18:55 -0500

    Dependency upgrades

--- src/test/java/com/daluga/messaging/producer/QueueProducerExampleApplicationTests.java
+++ src/test/java/com/daluga/messaging/producer/QueueProducerExampleApplicationTests.java
@@ -3,11 +3,9 @@ package com.daluga.messaging.producer;
 import com.daluga.messaging.consumer.QueueConsumerExampleApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = QueueConsumerExampleApplication.class)
 public class QueueProducerExampleApplicationTests {
 
 	@Test
